96c917e12d141640e7ad92b3201a68790c22abf9
jgabrielcybr <hollowfx35@gmail.com>
2026-02-10 21:37:26 -0800

    Update Assignment1_RectangleArea.cpp

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+
 using namespace std;
+
+
 
 int main()
 {
@@ -10,15 +13,21 @@ int main()
 	// get height and width from user
 	double height;
 	double width;
-	cout << "Enter height and width: ";
+	double area;
+
+	cout<<"Enter the height of the rectangle: "<<endl;
+	cin>>height;
+	cout<<"Enter the width of the rectangle: "<<endl;
+	cin>>width;
+
 /* write your code here */
 	// calculate area
-	
+	area = width * height;
+
 
 	// write output to console
-	
+	cout<<"The area of the rectangle is: "<<area<<endl;
 
 	// return value that indicates normal program exit
     return 0;
 }
-
