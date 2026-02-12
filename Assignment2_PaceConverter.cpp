201e4c9d2e622e3a9927f7714c1a5382fdd60321
jgabrielcybr <hollowfx35@gmail.com>
2026-02-11 21:32:21 -0800

    Update Assignment2_PaceConverter.cpp

--- Assignment2_PaceConverter.cpp
+++ Assignment2_PaceConverter.cpp
@@ -6,11 +6,15 @@ int main()
     double mph = 6.5;
     // PACE calculation
     /* write your code */
-
+    cout<<"How fast are you going in mph?"<<endl;
+    cin>>mph;
     //express pace in minutes and seconds
   /* write your code here */
-
+    double mpm = mph / 60;
+    double mps = mpm / 60;
   //express your output in 1 decimal points
+    cout<<"You are running "<<mpm<<" miles per minute!"<<endl;
+    cout<<"You are running "<<mps<<" miles per second!"<<endl;
 
-
+    return 0;
 }
